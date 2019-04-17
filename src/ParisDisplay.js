e35d2294d5a41795504a3abfc841b9b5a90f899f
antoinequellier <antoinecquellier@gmail.com>
2019-04-17 15:41:43 +0100

    destructuring text and icons in ParisDisplay component

--- src/ParisDisplay.js
+++ src/ParisDisplay.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import './ParisDisplay.css';
 
+const placeConfig = {
+  'paris': {
+    text: 'You are in Paris',
+    iconName: 'thumbs up'
+  },
+  'elsewhere': {
+    text: 'You are not in Paris',
+    iconName: 'thumbs down'
+  }
+}
 
 const getPlace = (lat, lng) => {
-  if (48.81 < lat < 48.9 && 2.27 < lng < 2.41) {
-    return "You are in Paris"
+  if (48.81 < lat && lat > 48.9 && 2.27 < lng && lng > 2.41) {
+    return "paris";
   } else {
-    return "You are not in Paris"
+    return "elsewhere";
   }
 }
 
 const ParisDisplay = props => {
-  return getPlace(props.latitude, props.longitude);
+  const place = getPlace(props.latitude, props.longitude);
+  const { text, iconName } = placeConfig[place];
+  return (
+    <div>
+      <i className={`icon ${iconName} massive icon-left` }></i>
+      <h1>{text}</h1>
+      <i className={`icon ${iconName} massive icon-right` }></i>
+    </div>
+  );
 }
 
 export default ParisDisplay;
