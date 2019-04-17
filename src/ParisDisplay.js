54079339b60de9f7cf77c1fd5d8fda84490bab24
antoinequellier <antoinecquellier@gmail.com>
2019-04-17 14:47:45 +0100

    passing props to ParisDisplay component

--- src/ParisDisplay.js
+++ src/ParisDisplay.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-const ParisDisplay = () => {
-  return (
-    <div>
-      Hi there!
-    </div>
-  )
+
+const getPlace = (lat, lng) => {
+  if (48.81 < lat < 48.9 && 2.27 < lng < 2.41) {
+    return "You are in Paris"
+  } else {
+    return "You are not in Paris"
+  }
+}
+
+const ParisDisplay = props => {
+  return getPlace(props.latitude, props.longitude);
 }
 
 export default ParisDisplay;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,25 @@ import ParisDisplay from './ParisDisplay';
 
 class App extends React.Component {
 
+  state = { lat: null, lng: null, errorMessage: '' }
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+
   render() {
     return(
-      <ParisDisplay/>
+      <ParisDisplay
+        latitude={this.state.lat}
+        longitude={this.state.lng}
+      />
     )
   }
 }
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
+
+// this.setState({lat: position.coords.latitude})
