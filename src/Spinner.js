edfa37ebeba8f38e279f2a2df70cbb780ac9db7f
antoinequellier <antoinecquellier@gmail.com>
2019-04-17 16:13:40 +0100

    make spinner dynamix

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
-const Spinner = () => {
+const Spinner = props => {
   return(
     <div className="ui active transition visible dimmer">
       <div className="content">
         <div className="ui text loader">
-          Loading...
+          {props.message}
         </div>
       </div>
     </div>
   );
+}
+
+Spinner.defaultProps = {
+  message: 'Loading...'
 }
 
 export default Spinner;
